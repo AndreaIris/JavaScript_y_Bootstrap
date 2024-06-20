9630a58b98ab42787fb4075d9a72abdb1728e7d2
AndreaIris <165727755+AndreaIris@users.noreply.github.com>
2024-06-20 19:11:36 -0300

    Version2

---
+++ scripts.js
@@ -0,0 +1,8 @@
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+   
+     $("#consultaForm").submit(function(event) {
+           event.preventDefault();
+           alert("Su consulta ha sido enviada. Nos pondremos en contacto con usted a la brevedad.");
+       });
+
+    
